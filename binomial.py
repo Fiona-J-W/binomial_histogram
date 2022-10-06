13fa4f8eb79784521a8cc271fca9a434223ee7eb
Florian Weber <oss@florianjw.de>
2022-10-06 17:09:35 +0100

    more space-efficient solution

--- binomial.py
+++ binomial.py
@@ -1,33 +1,43 @@
 #! /usr/bin/python3
 
+from math import factorial
+from functools import reduce
+from operator import mul
 from os import get_terminal_size
 from typing import Optional
 from fractions import Fraction
 import argparse
 
-def print_hist(n:int, p: Fraction, term_width: Optional[int] = None) -> None:
-    possibilities = [1]
-    ratios = [(1-p)**n]
-    for i in range(1, n+1):
-        pos_prev = possibilities[-1]
-        pos = pos_prev * (n-i+1) // (i)
-        ratio = ratios[-1] * pos * p / pos_prev / (1-p)
-        possibilities.append(pos)
-        ratios.append(ratio)
-    max_possibilities = possibilities[n//2]
-    max_ratio = max(ratios)
-    wi = len(str(n))
-    wn = len(str(max_possibilities))
+def choose(n: int, k: int) -> int:
+    assert n >= k >= 0
+    return reduce(mul, range(n-k+1, n+1), 1) // factorial(k)
+
+
+def get_width(term_width: Optional[int]) -> int:
     if term_width is None:
         try:
             term_width = get_terminal_size().columns
         except OSError:
             term_width = 100
-    hist_width = term_width - 27 - wi - wn
+    return term_width
+
+
+def print_hist(n:int, p: Fraction, term_width: Optional[int] = None) -> None:
+    wp = len(str(choose(n, n//2)))
+    wn = len(str(n))
+    mode = int(n*p + p)
+    max_ratio = choose(n, mode) * p**mode * (1-p)**(n-mode)
+    hist_width = get_width(term_width) - 27 - wp - wn
     accum_p = Fraction(0)
-    for i, (bc, ratio) in enumerate(zip(possibilities, ratios)):
+    pos = 1
+    ratio = (1-p)**n
+    for i in range(0, n+1):
+        if i > 0:
+            pos_prev = pos
+            pos = pos_prev * (n-i+1) // (i)
+            ratio = ratio * pos * p / pos_prev / (1-p)
         accum_p += ratio
-        print(f"{i:{wi}}: {bc:{wn}} {float(ratio):7.2%} {float(accum_p):7.2%} {float(1-accum_p):7.2%} " + '━' * round(hist_width * ratio/max_ratio))
+        print(f"{i:{wn}}: {pos:{wp}} {float(ratio):7.2%} {float(accum_p):7.2%} {float(1-accum_p):7.2%} " + '━' * round(hist_width * ratio/max_ratio))
 
 
 def main(args: list[str]) -> int:
